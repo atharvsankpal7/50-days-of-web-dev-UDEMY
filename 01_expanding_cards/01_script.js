95724a02acdca4889ad4447013d62ca1363e6dc0
atharvsankpal7 <atharvsankpal69420@gmail.com>
2022-11-23 23:11:00 +0530

    first commit

---
+++ 01_expanding_cards/01_script.js
@@ -0,0 +1,9 @@
+let panel = document.querySelectorAll(".panel");
+panel.forEach((e) => {
+    e.addEventListener("click", () => {
+        panel.forEach((e)=>{
+            e.classList.remove('active');
+        })
+        e.classList.add('active')
+    });
+});
